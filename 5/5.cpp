b97973d77b450319548658f40158aff482b5d88b
unknown <mfoysal314@gmail.com>
2022-08-25 02:33:14 +0600

    5 done

--- 5/5.cpp
+++ 5/5.cpp
@@ -2,7 +2,26 @@
 #include"MYSTACK.h"
 using namespace std;
 
+bool checkPalindrom(string str){
+    MyStack<char> stk;
+    string result = "";
+    for(int i=0;i<str.length();i++){
+        stk.push(str[i]);
+    }
+    while(!stk.isEmpty()){
+        result += stk.Top();
+        stk.pop();
+    }
+    if(str == result)return true;
+    else return false;
+}
+
 int main(){
+    string s;
+    cin>>s;
+    if(checkPalindrom(s)){
+        cout<<"YES"<<endl;
+    }else cout<<"NO"<<endl;
 
     return 0;
 }
