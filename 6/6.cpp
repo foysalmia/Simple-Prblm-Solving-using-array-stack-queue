531dfab0167628363510e1c09b754025d7a12f36
unknown <mfoysal314@gmail.com>
2022-08-25 02:39:08 +0600

    6 done

--- 6/6.cpp
+++ 6/6.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+#include"MYSTACK.h"
+using namespace std;
 
+void reverseString(string str){
+    MyStack<char> stk;
+    string res = "";
+    for(int i=0;i<str.length();i++){
+        stk.push(str[i]);
+    }
+    while(!stk.isEmpty()){
+        res += stk.Top();
+        stk.pop();
+    }
+    cout<<res<<endl;
+}
+
+int main(){
+    string a;
+    cin>>a;
+    reverseString(a);
+    return 0;
+}
